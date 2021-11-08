e26805880b658af03491a073e1e019060413433f
Devaure <daviddevaure@gmail.com>
2021-11-08 09:25:40 +0100

    my first commit

---
+++ Models/UserRolesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjPermManage.Models
+{
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+
+    }
+
+    public class ManagerUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public IList<UserRolesViewModel> UserRoles { get; set; }
+    }
+}
